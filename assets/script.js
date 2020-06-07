2d11dd0def3edaf05f9797807fcc9f6cda9615de
rhw <ruohanw89@gmail.com>
2020-06-07 12:43:42 -0700

    collected user inputs

--- assets/script.js
+++ assets/script.js
@@ -2,47 +2,85 @@
 
 // define password as an object with two properties length and character type.
 var passwordCriteria = {
-  length: '',
-  character: '',
-} ;
+  length: Number,
+  characterTypes: {
+    uppercase: Boolean,
+    lowercase: Boolean,
+    numeric: Boolean,
+    specialChar: Boolean,
+    defined: Boolean
+  }
+};
 
 // functions to collect requirements for the passwords
 // function of setting passord length
 var setLength = function(){
-  var length = window.prompt("Please set PASSWORD LENGHT (8 to 128 characters).");
-  // if user's input is a invalid number
-  if (length<8 || length>128 || length === null){
-    window.alert("Please provide a valid LENGHT. try again");
-    return setLength();
-    // if user's input is an valid number
-  }else{
-    passwordCriteria.length = length;
-    console.log(passwordCriteria.length);
-    return passwordCriteria.length;
+  var validatedInput = false;
+  while(!validatedInput){
+    var requireInput = true;
+      while(requireInput){
+        var length = window.prompt('Please set PASSWORD LENGHT (8 to 128 characters).');
+        // if user's input is a invalid number
+        requireInput = (isNaN(length) ||length<8 || length>128 || length === null);
+        if(requireInput){
+          window.alert('Please provide a valid LENGTH. try again');
+        }
+      }
+    validatedInput = window.confirm("Your password's length will be " + length +"?");
   }
+  passwordCriteria.length = parseInt(length);
+  console.log(passwordCriteria.length);
 };
-//function of selecting character types
 
+// // function to to randominze characters
+// var characterRandom = function(){
+//   var value = Math.floor(Math.random()*);
+
+// };
+
+//function of selecting character types
+var setCharacterTypes = function(){
+  var validatedInput = false;
+  while(!validatedInput){
+    var requireInput = true;
+    while(requireInput){
+      var uppercase = window.confirm('Do you want UPPERCASE LETTERS in your password? Ok for yes. Cancel for no.');
+      var lowercase = window.confirm('Do you want LOWERCASE LETTERS in your password? Ok for yes. Cancel for no.');
+      var numeric= window.confirm('Do you want NUMERIC CHARACTER in your password? Ok for yes. Cancel for no.');
+      var specialChar = window.confirm('Do you want SPECIAL CHARACTER in your password? Ok for yes. Cancel for no.');
+      requireInput = (!uppercase && !lowercase && !numeric && !specialChar);
+      if(requireInput){
+        window.alert("No charater types are chosen! Please choose at least one.");
+      }
+    }
+    var confirmMessage = "The following character types are chosen: ";
+    if(uppercase){
+      confirmMessage = confirmMessage + "Uppercase Letters; ";
+    }
+    if(lowercase){
+      confirmMessage = confirmMessage + "Lowercase Letters; ";
+    }
+    if(numeric){
+      confirmMessage = confirmMessage + "Numbers; ";
+    }
+    if(specialChar){
+      confirmMessage = confirmMessage + "Special Characters; ";
+    }
+    validatedInput = window.confirm(confirmMessage);
+  };
+  // record input to object
+  passwordCriteria.characterTypes.uppercase = uppercase;
+  passwordCriteria.characterTypes.lowercase = lowercase;
+  passwordCriteria.characterTypes.numeric = numeric;
+  passwordCriteria.characterTypes.specialChar = specialChar;
+}
 
 // function to present with a series of prompts for password criteria
 // window prompt to select which criteria to set
-var generatePassword = function(){
-  debugger;
-  var selectCriteria = window.prompt("Please select criteria to include in your new password. Type 1 for setting password LENGHT. Type 2 for setting password CHARACTER TYPE.");
-  switch(parseInt(selectCriteria)){
-    case 1:
-      setLength();
-      break;
-    case 2:
-      setCharacterTypes();
-      break;
-    default:
-      window.alert("You did not pick a valid option. Try again.");
-      // call itself again
-      generatePassword();
-  }
+var generatePassword = function(){ 
+  setLength();
+  setCharacterTypes();
 }
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
